e27fb7f13878e9cc6ae5fa7892944bccb573ed5d
Nikolay <Onedaydejavu124@mail.ru>
2022-12-22 00:27:12 +0700

    Циклы

---
+++ Циклы.py/for.py
@@ -0,0 +1,19 @@
+#for i in 1,2,3,4,5:
+#    print(i**2)
+
+#list = [1,2,3,4,5]
+#for i in list:
+#    print(i**2)
+
+#r = range(10) # перечисление
+#for i in r:
+#    print(i)
+
+#for j in range(1,5):
+#    print(j) 
+
+#for k in range(1,10,2):
+#    print(k)
+
+for i in "qwe - rty":
+    print(i)
