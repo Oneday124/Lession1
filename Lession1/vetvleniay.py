13b49080a8ebfd353849494694257c41c062c15a
Nikolay <Onedaydejavu124@mail.ru>
2022-12-24 23:32:56 +0700

    Лекция 1.

---
+++ Lession1/vetvleniay.py
@@ -0,0 +1,21 @@
+# Управляющие конструкции if, if - else
+
+a = int(input('a = '))
+b = int(input('b = '))
+if a > b:
+    print(a)
+else:
+    print(b)
+
+
+username = input("Введите имя: ")
+if username == "Маша":
+    print("Ура, это же Маша!")
+elif username == "Марина":
+    print("Ура, это же Марина!")
+elif username == "Ильнар":
+    print("Ильнар - ТОП!")
+else:
+    print("Привет, ", username)    
+
+
