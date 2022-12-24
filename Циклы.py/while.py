96057e01d67d8203139a780a40bfc92ff3123d6c
Nikolay <Onedaydejavu124@mail.ru>
2022-12-24 23:31:02 +0700

    Лекция 1.

--- Циклы.py/while.py
+++
@@ -1,9 +0,0 @@
-
-original = 23
-inverted = 0
-while original != 0:
-    inverted = inverted * 10 + (original % 10)
-    original //= 10
-else:
-    print("Пожалуй, хватит") # Когда условие перестает выполняться
-print(inverted)
