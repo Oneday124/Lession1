1d9c2424c7a7cc6715e4419955088a23c7b5e7bc
Nikolay <Onedaydejavu124@mail.ru>
2023-04-09 14:37:20 +0700

    Задачи на рекурсию

--- Seminars/Seminar5/EX001_1.py
+++ Seminars/Seminar5/EX001_1.py
@@ -38,6 +38,6 @@
 # найти сумму цифр числа
 def sum_number(n: int):
     if (n // 10) == 0:
-        return n%10
+        return n % 10
     return sum_number(n//10) + n % 10
 print(sum_number(2122))
